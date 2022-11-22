185199d498c4e5e7dc2c6b71a26033f540287c7c
alaidine <aa.alimlinde@gmail.com>
2022-11-22 19:17:47 +0400

    changed and added new keymaps

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -31,6 +31,13 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+-- Move window
+keymap.set("n", "<Space>", "<C-w>w")
+keymap.set("", "zh", "<C-w>h")
+keymap.set("", "zk", "<C-w>k")
+keymap.set("", "zj", "<C-w>j")
+keymap.set("", "zl", "<C-w>l")
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
@@ -43,6 +50,7 @@ keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>")
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
