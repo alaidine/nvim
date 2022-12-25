27a8f11d89575110cd6c56751b239fa22280b138
alaidine <aa.alimlinde@gmail.com>
2022-12-25 22:19:13 +0400

    created the file with options, the file with plugins and the file with the keymaps

---
+++ lua/alaidine/base.lua
@@ -0,0 +1,16 @@
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+
+vim.laststatus = 2
+vim.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false
+vim.opt.shell = 'zsh'
+vim.cmdheight = 1
