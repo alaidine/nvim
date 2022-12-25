f8b2ef76a0f63a7435b81b6140ceb00cbe80d06a
alaidine <aa.alimlinde@gmail.com>
2022-12-26 01:09:48 +0400

    configured the new plugins

---
+++ after/plugin/dashboard.lua
@@ -0,0 +1,81 @@
+local db = require('dashboard')
+
+local home = os.getenv('HOME')
+
+db.default_banner = {
+
+  '',
+
+  '',
+
+  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+
+  '',
+
+  ' [ TIP: To exit Neovim, just power off your computer. ] ',
+
+  '',
+
+}
+
+-- linux
+
+--db.preview_command = 'ueberzug'
+
+--
+
+--db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
+
+db.preview_file_height = 11
+
+db.preview_file_width = 70
+
+db.custom_center = {
+    {icon = '  ',
+
+    desc = 'Find recent files                       ',
+
+    action = 'Telescope oldfiles',
+
+    shortcut = 'SPC f r'},
+
+    {icon = '  ',
+
+    desc = 'Find files                              ',
+
+    action = 'Telescope find_files find_command=rg,--hidden,--files',
+
+    shortcut = 'SPC f f'},
+
+    {icon = '  ',
+
+    desc ='File browser                            ',
+
+    action =  'Telescope file_browser',
+
+    shortcut = 'SPC f b'},
+
+    {icon = '  ',
+
+    desc = 'Find word                               ',
+
+    action = 'Telescope live_grep',
+
+    shortcut = 'SPC f w'},
+
+    {icon = '  ',
+
+    desc = 'Load new theme                          ',
+
+    action = 'Telescope colorscheme',
+
+    shortcut = 'SPC h t'},
+
+  }
+
+db.custom_footer = { '', 'If I\'m using Neovim, then my Emacs config must be broken!' }
