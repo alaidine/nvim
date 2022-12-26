75cb1c05078117839af6305f85b466123d7a04a3
alaidine <aa.alimlinde@gmail.com>
2022-12-26 21:25:08 +0400

    set tabstop to 2

--- lua/alaidine/base.lua
+++ lua/alaidine/base.lua
@@ -1,22 +1,21 @@
 local set = vim.opt -- set options
 
-vim.scriptencoding = 'utf-8'
-set.encoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+set.encoding = "utf-8"
 
 vim.wo.number = true
 vim.wo.relativenumber = true
 
-set.autoindent = true
-set.smartindent = true
-set.tabstop = 2
-set.softtabstop = 2
-set.expandtab = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
 set.hlsearch = true
 vim.shiftwidth = 2
 vim.laststatus = 2
 set.wrap = false
-set.shell = 'zsh'
+set.shell = "zsh"
 vim.cmdheight = 1
 set.scrolloff = 10
 
@@ -28,3 +27,19 @@ set.cursorline = true
 set.termguicolors = true
 set.background = "dark"
 set.signcolumn = "yes"
+
+-- highlights
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = "pum"
+vim.opt.pumblend = 5
+vim.opt.background = "dark"
+
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd([[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+  augroup END
+]])
