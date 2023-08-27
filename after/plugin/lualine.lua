aa6b2396bc0257f34b115a4a8de272cdfed45a19
alaidine <aa.alimlinde@gmail.com>
2023-08-27 22:35:08 +0400

    removed some useless things from the status line

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -19,7 +19,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'branch'},
     lualine_c = {'buffers'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
