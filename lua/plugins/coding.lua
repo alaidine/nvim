ab68ff76393745a1d7b6979c1222d903dcbc9734
alaidine <aa.alimlinde@gmail.com>
2024-06-15 19:14:14 +0400

    add tmux plugin

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -25,4 +25,13 @@ return {
       -- or leave it empty to use the default settings
     },
   },
+
+  {
+    "folke/twilight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
