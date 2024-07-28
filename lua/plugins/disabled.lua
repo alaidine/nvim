51da43740e75a2d80930d8371798d9f2d5a54ff6
alaidine <aa.alimlinde@gmail.com>
2024-07-28 20:31:05 +0400

    add indent-blankline to disabled

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "ellisonleao/gruvbox.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false }
 }
