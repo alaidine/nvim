87d8e4e3f3e9e34a785d7610d8610502c7c4eb8a
aalaaiddine alimlinde <aalaaiddine.alimlinde@cg974.fr>
2024-07-31 15:06:46 +0400

    change colorscheme and lualine stuff

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "folke/tokyonight.nvim", enabled = false },
-  { "ellisonleao/gruvbox.nvim", enabled = false },
+  { "ellisonleao/gruvbox.nvim", enabled = true },
   { "echasnovski/mini.indentscope", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false }
 }
