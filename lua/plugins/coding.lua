ae9a789635756f46ad01e8c67f57d0d26ae6cd46
alaidine <aa.alimlinde@gmail.com>
2025-02-06 10:53:04 +0400

    add config for rust and add lua formatter config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,4 +1,13 @@
 return {
-  { 'nmac427/guess-indent.nvim', opts = {} },
-  { 'nvim-lua/plenary.nvim' }
+  { "nmac427/guess-indent.nvim", opts = {} },
+  { "nvim-lua/plenary.nvim" },
+  {
+    "rust-lang/rust.vim",
+    config = function()
+      vim.cmd([[
+  syntax enable
+  filetype plugin indent on
+]])
+    end,
+  },
 }
